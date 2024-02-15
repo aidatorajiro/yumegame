c75aba496f92a3e18ed6a595e26dc639f972c425
aidatorajiro <kawarusosu@zoho.com>
2024-02-15 23:41:57 +0900

    frp journey: learned hold and monad operations on events

--- hs/yumegame/src/Logic.hs
+++ hs/yumegame/src/Logic.hs
@@ -18,9 +18,11 @@ data Innerworld = Innerworld { script :: [S.ByteString], timestamp :: Double }
 yaruzoo :: SF Outerworld Innerworld
 yaruzoo = proc x -> do
   let sdlEvs = sdlEvents x
+  lastControllerEv <- hold NoEvent -< (if null sdlEvs then NoEvent else Event (Event (last sdlEvs)))
   ev <- repeatedly 1 () -< ()
+  let controllerEvPerSec = ev >> lastControllerEv
   t <- time -< ()
   let scr = case ev of 
-        Event _ -> ["print(" <> S.fromString (show t) <> ")"]
+        Event _ -> ["print('" <> S.fromString (show controllerEvPerSec) <> "')"]
         NoEvent -> []
   returnA -< Innerworld { script = scr, timestamp = 0 }
