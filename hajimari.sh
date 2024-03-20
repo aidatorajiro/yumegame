494aa760840c0154acdca98d8b421662746b9d52
aidatorajiro <kawarusosu@zoho.com>
2024-03-21 04:49:22 +0900

    add some comment

--- hajimari.sh
+++ hajimari.sh
@@ -13,15 +13,15 @@ xfce4-terminal --command='blender' --title='YUME develop: console B'
 cat startup.py | xclip -selection clipboard
 sleep 5
 
-# 0: Debug / Sub Code
+# Workspace 0: Debug / Sub Code
 wmctrl -r "YUME develop: console A" -t 0
 wmctrl -r "YUME develop: console B" -t 0
 wmctrl -r "yumegame - Visual Studio Code" -t 0
-# 1: Blender
+# Workspace 1: Blender   TODO: a little bit of danger of misclassification
 wmctrl -r "- Blender " -t 1
 wmctrl -r "- Blender " -b add,fullscreen
-# 2: Main Code
+# Workspace 2: Main Code
 wmctrl -r "yumegamehs - Visual Studio Code" -t 2
 wmctrl -r "yumegamehs - Visual Studio Code" -b add,fullscreen
-# 3: Browse
+# Workspace 3: Browse   TODO: a little bit of danger of misclassification
 wmctrl -r "Firefox" -t 3
