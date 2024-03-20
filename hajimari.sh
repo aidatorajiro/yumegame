2b13aa65c9afb39c10b049569ffbd64fb1f1983c
aidatorajiro <kawarusosu@zoho.com>
2024-03-21 04:47:02 +0900

    fix firefox issue for autostart script

--- hajimari.sh
+++ hajimari.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+function dae () {
+    setsid $@ >/dev/null 2>&1 < /dev/null &
+}
+
 sh ./start_ide_ghcup.sh
 code .
-firefox --ProfileManager &
+dae firefox --ProfileManager
 xfce4-terminal --command='sh ./start_watch_ghcup.sh' --title='YUME develop: console A'
 xfce4-terminal --command='blender' --title='YUME develop: console B'
-cat startup.py | xclip -selection clipboard
 
+cat startup.py | xclip -selection clipboard
 sleep 5
 
 # 0: Debug / Sub Code
@@ -15,7 +19,9 @@ wmctrl -r "YUME develop: console B" -t 0
 wmctrl -r "yumegame - Visual Studio Code" -t 0
 # 1: Blender
 wmctrl -r "- Blender " -t 1
+wmctrl -r "- Blender " -b add,fullscreen
 # 2: Main Code
 wmctrl -r "yumegamehs - Visual Studio Code" -t 2
+wmctrl -r "yumegamehs - Visual Studio Code" -b add,fullscreen
 # 3: Browse
 wmctrl -r "Firefox" -t 3
