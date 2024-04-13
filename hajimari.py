fa6a93b077c94efbd8e02b1d7ea6138997aa8000
aidatorajiro <kawarusosu@zoho.com>
2024-04-13 16:10:07 +0900

    devenv: hajimari.py: add obs and refactor

--- hajimari.py
+++ hajimari.py
@@ -1,14 +1,25 @@
 #!/bin/python3
+"""
+Arch Linux: 2024-04-13
 
-# Arch Linux: 2024-03-21
-# linux: 6.8.1.arch1-1
-# util-linux: 2.39.3-2
-# wmctrl: 1.07-6
-# xclip: 0.13-4
-# xfce4-session: 4.18.3-1
-# xfce4-terminal: 1.1.3-1
-# blender: 17:4.0.2-21
-# firefox: 124.0-1
+pacman -Q | rg "^(linux|util-linux|wmctrl|xclip|xfce4-session|xfce4-terminal|blender|firefox|obs-studio)"
+
+blender 17:4.1.0-3
+firefox 124.0.2-1
+linux 6.8.5.arch1-1
+linux-api-headers 6.7-1
+linux-firmware 20240409.1addd7dc-1
+linux-firmware-whence 20240409.1addd7dc-1
+linux-headers 6.8.5.arch1-1
+obs-studio 30.1.1-1
+util-linux 2.40-2
+util-linux-libs 2.40-2
+wmctrl 1.07-6
+xclip 0.13-4
+xfce4-session 4.18.3-1
+xfce4-terminal 1.1.3-1
+"""
+
 
 import subprocess
 import os
@@ -24,38 +35,44 @@ WAIT_SECONDS_APPS_OPEN = 10
 WAIT_SECONDS_WINDOW_MOVE = 0.1
 DESKTOP_CONFIG = [[0, 27, 1910, 1016], [1920, 0, 1910, 1046]] # manually obtained x/y/w/h for a window that covers whole display (DESKTOP_CONFIG[0] will be of left display, DESKTOP_CONFIG[1] will be of right display)
 
-def move_window_workspace(title, desktop, wait=True):
-    subprocess.run(["wmctrl", "-r", title, "-t", desktop])
+def class_option(classname):
+    if classname:
+        return ["-x"]
+    else:
+        return []
+
+def move_window_workspace(title, classname, desktop, wait=True):
+    subprocess.run(["wmctrl"] + class_option(classname) + ["-r", title, "-t", desktop])
     if wait:
         time.sleep(WAIT_SECONDS_WINDOW_MOVE)
 
-def move_window_position(title, config, wait=True):
+def move_window_position(title, classname, config, wait=True):
     [gravity, pos_x, pos_y, width, height] = config
     arg = ",".join([str(gravity), str(pos_x), str(pos_y), str(width), str(height)])
-    subprocess.run(["wmctrl", "-r", title, "-e", arg])
+    subprocess.run(["wmctrl"] + class_option(classname) + ["-r", title, "-e", arg])
     if wait:
         time.sleep(WAIT_SECONDS_WINDOW_MOVE)
-    subprocess.run(["wmctrl", "-r", title, "-e", arg])
-    if wait:
-        time.sleep(WAIT_SECONDS_WINDOW_MOVE)
-
-def make_fullscreen(title, wait=True):
-    subprocess.run(["wmctrl", "-r", title, "-b", "add,fullscreen"])
+    subprocess.run(["wmctrl"] + class_option(classname) + ["-r", title, "-e", arg])
     if wait:
         time.sleep(WAIT_SECONDS_WINDOW_MOVE)
 
-def remove_fullscreen(title, wait=True):
-    subprocess.run(["wmctrl", "-r", title, "-b", "remove,fullscreen"])
+def make_fullscreen(title, classname, wait=True):
+    subprocess.run(["wmctrl"] + class_option(classname) + ["-r", title, "-b", "add,fullscreen"])
     if wait:
         time.sleep(WAIT_SECONDS_WINDOW_MOVE)
 
-def make_sticky(title, wait=True):
-    subprocess.run(["wmctrl", "-r", title, "-b", "add,sticky"])
+def remove_fullscreen(title, classname, wait=True):
+    subprocess.run(["wmctrl"] + class_option(classname) + ["-r", title, "-b", "remove,fullscreen"])
     if wait:
         time.sleep(WAIT_SECONDS_WINDOW_MOVE)
 
-def remove_sticky(title, wait=True):
-    subprocess.run(["wmctrl", "-r", title, "-b", "remove,sticky"])
+def make_sticky(title, classname, wait=True):
+    subprocess.run(["wmctrl"] + class_option(classname) + ["-r", title, "-b", "add,sticky"])
+    if wait:
+        time.sleep(WAIT_SECONDS_WINDOW_MOVE)
+
+def remove_sticky(title, classname, wait=True):
+    subprocess.run(["wmctrl"] + class_option(classname) + ["-r", title, "-b", "remove,sticky"])
     if wait:
         time.sleep(WAIT_SECONDS_WINDOW_MOVE)
 
@@ -74,13 +91,16 @@ def switch_desktop(s, wait=True):
     if wait:
         time.sleep(WAIT_SECONDS_WINDOW_MOVE)
 
-def remove_maximized(title, wait=True):
-    subprocess.run(["wmctrl", "-r", title, "-b", "remove,maximized_vert"])
-    subprocess.run(["wmctrl", "-r", title, "-b", "remove,maximized_horz"])
+def remove_maximized(title, classname, wait=True):
+    subprocess.run(["wmctrl"] + class_option(classname) + ["-r", title, "-b", "remove,maximized_vert"])
+    subprocess.run(["wmctrl"] + class_option(classname) + ["-r", title, "-b", "remove,maximized_horz"])
     if wait:
         time.sleep(WAIT_SECONDS_WINDOW_MOVE)
 
 def main_spawn_processes():
+    # main ide
+    subprocess.Popen(["setsid", "obs"], stdout=subprocess.DEVNULL, stdin=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
     # main ide
     subprocess.Popen(["sh", "start_ide_ghcup.sh"])
 
@@ -88,7 +108,7 @@ def main_spawn_processes():
     subprocess.Popen(["code" ,"."])
 
     # firefox, make sure the process is completely separated from the parent
-    subprocess.Popen(["setsid" ,"firefox", "-P", "yumegame develop"], stdout=subprocess.DEVNULL, stdin=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    subprocess.Popen(["setsid", "firefox", "-P", "yumegame develop"], stdout=subprocess.DEVNULL, stdin=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
     # build log console
     subprocess.Popen([TERMINAL, "--command=sh ./start_watch_ghcup.sh", "--title=YUME develop: console A"])
@@ -103,65 +123,96 @@ def main_spawn_processes():
     # wait for some seconds
     time.sleep(WAIT_SECONDS_APPS_OPEN)
 
+def place_blender(desktop, single):
+    title = "Blender.Blender"
+    classname = True
+    remove_fullscreen(title, classname)
+    remove_maximized(title, classname)
+    move_window_workspace(title, classname, desktop)
+    if single:
+        move_window_position(title, classname, calc_config(0, 1, 1, 0, 0, 1, 1))
+    else:
+        make_sticky(title, classname)
+        move_window_position(title, classname, calc_config(1, 1, 1, 0, 0, 1, 1))
+    make_fullscreen(title, classname)
+
+def place_console_a(desktop):
+    title = "YUME develop: console A"
+    classname = False
+    remove_fullscreen(title, classname)
+    remove_maximized(title, classname)
+    move_window_workspace(title, classname, desktop)
+    move_window_position(title, classname, calc_config(0, 2, 2, 0, 0, 1, 1))
+
+def place_console_b(desktop):
+    title = "YUME develop: console B"
+    classname = False
+    remove_fullscreen(title, classname)
+    remove_maximized(title, classname)
+    move_window_workspace(title, classname, desktop)
+    move_window_position(title, classname, calc_config(0, 2, 2, 0, 1, 1, 1))
+
+def place_code_devenv(desktop):
+    title = "yumegame - Visual Studio Code"
+    classname = False
+    remove_fullscreen(title, classname)
+    remove_maximized(title, classname)
+    move_window_workspace(title, classname, desktop)
+    move_window_position(title, classname, calc_config(0, 2, 2, 1, 0, 1, 2))
+
+def place_code_hs(desktop):
+    title = "yumegamehs - Visual Studio Code"
+    classname = False
+    remove_fullscreen(title, classname)
+    remove_maximized(title, classname)
+    move_window_workspace(title, classname, desktop)
+    move_window_position(title, classname, calc_config(0, 1, 1, 0, 0, 1, 1))
+    make_fullscreen(title, classname)
+
+def place_ff(desktop):
+    title = "Navigator.firefox"
+    classname = True
+    remove_fullscreen(title, classname)
+    remove_maximized(title, classname)
+    move_window_workspace(title, classname, desktop)
+    move_window_position(title, classname, calc_config(0, 1, 1, 0, 0, 1, 1))
+    make_fullscreen(title, classname)
+
+def place_obs(desktop):
+    title = "obs.obs"
+    classname = True
+    remove_fullscreen(title, classname)
+    remove_maximized(title, classname)
+    move_window_workspace(title, classname, desktop)
+    move_window_position(title, classname, calc_config(0, 1, 1, 0, 0, 1, 1))
+    make_fullscreen(title, classname)
+
 def main_placement(single=False):
     # Workspace 0: Blender   TODO: a little bit of danger of misclassification
     desktop = '0'
     switch_desktop(desktop)
-
-    title = "- Blender 4."
-    remove_fullscreen(title)
-    remove_maximized(title)
-    move_window_workspace(title, desktop)
-    if single:
-        move_window_position(title, calc_config(0, 1, 1, 0, 0, 1, 1))
-    else:
-        make_sticky(title)
-        move_window_position(title, calc_config(1, 1, 1, 0, 0, 1, 1))
-    make_fullscreen(title)
+    place_blender(desktop, single)
 
     # Workspace 1: Debug / Sub Code
     desktop = '1'
     switch_desktop(desktop)
-
-    title = "YUME develop: console A"
-    remove_fullscreen(title)
-    remove_maximized(title)
-    move_window_workspace(title, desktop)
-    move_window_position(title, calc_config(0, 2, 2, 0, 0, 1, 1))
-
-    title = "YUME develop: console B"
-    remove_fullscreen(title)
-    remove_maximized(title)
-    move_window_workspace(title, desktop)
-    move_window_position(title, calc_config(0, 2, 2, 0, 1, 1, 1))
-
-    title = "yumegame - Visual Studio Code"
-    remove_fullscreen(title)
-    remove_maximized(title)
-    move_window_workspace(title, desktop)
-    move_window_position(title, calc_config(0, 2, 2, 1, 0, 1, 2))
+    place_console_a(desktop)
+    place_console_b(desktop)
+    place_code_devenv(desktop)
 
     # Workspace 2: Main Code
     desktop = '2'
     switch_desktop(desktop)
-
-    title = "yumegamehs - Visual Studio Code"
-    remove_fullscreen(title)
-    remove_maximized(title)
-    move_window_workspace(title, desktop)
-    move_window_position(title, calc_config(0, 1, 1, 0, 0, 1, 1))
-    make_fullscreen(title)
+    place_code_hs(desktop)
 
     # Workspace 3: Browse   TODO: a little bit of danger of misclassification
     desktop = '3'
     switch_desktop(desktop)
+    place_ff(desktop)
 
-    title = "Firefox"
-    remove_fullscreen(title)
-    remove_maximized(title)
-    move_window_workspace(title, desktop)
-    move_window_position(title, calc_config(0, 1, 1, 0, 0, 1, 1))
-    make_fullscreen(title)
+    desktop = '4'
+    switch_desktop(desktop)
+    place_obs(desktop)
 
     # Turns out that this can be done using add,sticky...
     """
