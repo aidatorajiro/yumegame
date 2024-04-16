fed8313efd0ed5542d1767167908ace7e14d3469
aidatorajiro <kawarusosu@zoho.com>
2024-04-16 16:31:48 +0900

    util: remove unnecessary envcopy

--- mount.py
+++ mount.py
@@ -4,8 +4,6 @@ import subprocess
 
 import os
 
-envcpy = os.environ.copy()
-
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-subprocess.run(["restic", "-r", "rclone:ueno:yumegame", "mount", "./mnt", '--password-file', 'password-file'], env=envcpy)
+subprocess.run(["restic", "-r", "rclone:ueno:yumegame", "mount", "./mnt", '--password-file', 'password-file'])
--- restore.py
+++ restore.py
@@ -4,8 +4,6 @@ import subprocess
 
 import os
 
-envcpy = os.environ.copy()
-
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-subprocess.run(["restic", "-r", "rclone:ueno:yumegame", "restore", "latest", "--target", ".", '--password-file', 'password-file', '-e', '.git'], env=envcpy)
+subprocess.run(["restic", "-r", "rclone:ueno:yumegame", "restore", "latest", "--target", ".", '--password-file', 'password-file', '-e', '.git'])
