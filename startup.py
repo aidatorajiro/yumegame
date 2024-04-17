21135d90a63724b8d7664743ea98697b84e29411
aidatorajiro <kawarusosu@zoho.com>
2024-04-17 16:16:44 +0900

    startup script: set viewport to rendered and make fullscreen automatically

--- startup.py
+++ startup.py
@@ -3,7 +3,18 @@
 import bpy
 import os
 
+def get_main_area():
+    for a in bpy.context.screen.areas:
+        if a.spaces.active and a.spaces.active.type == 'VIEW_3D':
+            return a
+
+def startup_misc():
+    get_main_area().spaces.active.shading.type = "RENDERED"
+    bpy.ops.wm.window_fullscreen_toggle()
+
 proj_path = bpy.path.abspath("//")
 
 with open(os.path.join(proj_path, "yumepy", "main.py")) as f:
     exec(f.read())
+
+startup_misc()
