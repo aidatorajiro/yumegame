16b78a31aa8785629f60f3716011dd8d5bb0e4a1
tora <kawarusosu@zoho.com>
2025-03-22 17:53:02 +0900

    update ghc to 9.6.6

--- yumepy/util.py
+++ yumepy/util.py
@@ -1,5 +1,7 @@
 import os
 
+GHCUP_GHC_VERSION = "9.6.6"
+
 base_path = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 
 def calculate_env():
@@ -7,9 +9,9 @@ def calculate_env():
 
     if "ghcup" in os.environ["BUILD_METHOD"]:
         if "GHCUP_PATH" in os.environ:
-            newenv["PATH"] = newenv["GHCUP_PATH"] + os.pathsep + newenv["PATH"]
+            newenv["PATH"] = os.path.join(newenv["GHCUP_PATH"], "ghc", GHCUP_GHC_VERSION, "bin") + os.pathsep + os.path.join(newenv["GHCUP_PATH"], "bin") + os.pathsep + newenv["PATH"]
         else:
-            newenv["PATH"] = os.path.join(os.path.expanduser('~'), ".ghcup", "bin") + os.pathsep + newenv["PATH"]
+            newenv["PATH"] = os.path.join(os.path.expanduser('~'), ".ghcup", "ghc", GHCUP_GHC_VERSION, "bin") + os.pathsep + os.path.join(os.path.expanduser('~'), ".ghcup", "bin") + os.pathsep + newenv["PATH"]
     
     if "nix" in os.environ["BUILD_METHOD"]:
         if not os.path.exists(os.path.join(base_path, "nix_env_export")):
